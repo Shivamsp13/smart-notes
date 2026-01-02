49ecf49c66ef8802e39fe71305bf7937465ed67d
Shivamsp13 <shivamofficialsp13@gmail.com>
2026-01-02 11:16:21 +0530

    Main SmartNotesApplication.java

--- src/main/java/com/shivam/smartnotes/SmartNotesApplication.java
+++ src/main/java/com/shivam/smartnotes/SmartNotesApplication.java
@@ -2,7 +2,9 @@ package com.shivam.smartnotes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 ////mvn clean compile->use this to check the condition of the app.
 @SpringBootApplication
@@ -10,5 +12,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class SmartNotesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SmartNotesApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }

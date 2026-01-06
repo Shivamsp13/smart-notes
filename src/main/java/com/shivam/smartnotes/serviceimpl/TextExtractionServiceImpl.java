7f6bcbc2d770d5e2e97ed858692f545c8e2414bf
Shivamsp13 <shivamofficialsp13@gmail.com>
2026-01-06 22:20:04 +0530

    UPDATED: 1)TextExtractionServiceImpl

--- src/main/java/com/shivam/smartnotes/serviceimpl/TextExtractionServiceImpl.java
+++ src/main/java/com/shivam/smartnotes/serviceimpl/TextExtractionServiceImpl.java
@@ -12,8 +12,13 @@ import java.io.InputStream;
 @Service
 public class TextExtractionServiceImpl implements TextExtractionService {
 
+    private static final long MAX_FILE_SIZE_BYTES = 50 * 1024 * 1024; // 500 MB
+    private static final int MAX_PAGE_COUNT = 100;
+
     @Override
     public String extractText(MultipartFile file) {
+
+
         if(file==null || file.isEmpty()){
             throw new IllegalArgumentException("Uploaded file is Empty");
         }
@@ -21,9 +26,23 @@ public class TextExtractionServiceImpl implements TextExtractionService {
             throw new IllegalArgumentException("Only PDF files are supported");
         }
 
+        if (file.getSize() > MAX_FILE_SIZE_BYTES) {
+            throw new IllegalArgumentException(
+                    "PDF file size exceeds limit of 50 MB"
+            );
+        }
+
         try(InputStream inputStream=file.getInputStream();
             PDDocument document=PDDocument.load(inputStream)
             ) {
+
+            int pageCount = document.getNumberOfPages();
+            if (pageCount > MAX_PAGE_COUNT) {
+                throw new IllegalArgumentException(
+                        "PDF has too many pages. Maximum allowed is " + MAX_PAGE_COUNT
+                );
+            }
+
             PDFTextStripper stripper = new PDFTextStripper();
             String extractedText = stripper.getText(document);
 
